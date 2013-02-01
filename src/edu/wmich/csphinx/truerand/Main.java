18c131238bd3e3682e5c50cfe8d44c639c2a2d54
Chris Sphinx <chrissphinx@gmail.com>
2013-02-01 12:34:20 -0500

    Initial commit

---
+++ src/edu/wmich/csphinx/truerand/Main.java
@@ -0,0 +1,14 @@
+package edu.wmich.csphinx.truerand;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception {
+		Random rand = new Random();
+		for (int j = 0; j < 10; j++) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print(rand.nextInt(0, 9) + " ");
+			} System.out.println();
+		}
+		rand.close();
+	}
+}
---
+++ src/edu/wmich/csphinx/truerand/Random.java
@@ -0,0 +1,65 @@
+package edu.wmich.csphinx.truerand;
+
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.net.URL;
+
+public class Random
+{
+	private final int NUM_BYTES = 1250;
+	private File file = new File("random.dat");
+	private RandomAccessFile ram = new RandomAccessFile(file, "rw");
+	private long readpt, writept;
+	
+	public Random() throws IOException {
+		ram.seek(0);
+		readpt = ram.readLong();		// pointer for reading bytes
+		writept = ram.readLong();		// pointer for writing bytes
+		ram.seek(readpt);				// resume reading file
+	}
+	
+	public int nextInt(int l, int h) throws IOException {
+		int m = (h - l) + 1;			// number of ranges needed
+		int r = 256 / m;				// size of byte range
+		int x = (r * m) - 1;			// maximum allowable byte value
+		int b;
+		do {
+			try {						// get random byte from file
+				b = ram.readUnsignedByte();
+			} catch (EOFException e) {	// catch EOF, reset pointer
+				b = 255; ram.seek(16);	// and set b to maximum value
+			}							// so test will fail.
+		} while(b > x);					// if byte is greater than
+		readpt = ram.getFilePointer();	// allowable value, loop.
+		return (b / r) + l;				// return random integer
+	}									// within requested range
+	
+	public void close() throws IOException {
+		ram.seek(0);					// move pointer to start of file
+		ram.writeLong(readpt);			// save the read and write
+		ram.writeLong(writept);			// pointer and close the
+		ram.close();					// file
+	}
+	
+	public void newBytes() throws IOException {
+		URL url = new URL("http://www.random.org/integers/?num=" + (NUM_BYTES * 8) + "&min=0&max=1&col=8&base=2&format=plain&rnd=new");
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(url.openStream()));
+		byte[] bytes = new byte[NUM_BYTES];		
+		
+		try {
+			String line; int i = 0;
+			while((line = buffer.readLine()) != null) {
+				bytes[i++] = (byte) Integer.parseInt(line.replaceAll("\\s+", ""), 2);
+			}
+			ram.seek(writept);
+			ram.write(bytes);
+		} catch (Exception e) {
+		} finally {
+			writept = ram.getFilePointer(); buffer.close();
+		}
+	}
+}
