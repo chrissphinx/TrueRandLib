f7224c644ec2a46da74e4a8996591ea719710130
Chris Sphinx <chrissphinx@gmail.com>
2013-02-01 22:43:01 -0500

    Simplified folder structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package edu.wmich.csphinx.truerand;
-
 public class Main
 {
 	public static void main(String[] args) throws Exception {
--- src/Random.java
+++ src/Random.java
@@ -1,5 +1,3 @@
-package edu.wmich.csphinx.truerand;
-
 import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
